5001c7e38ce49b50473034806fcdf4e5c496bfbf
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2025-12-18 15:57:52 +0000

    comments

--- test/test-builds.sh
+++ test/test-builds.sh
@@ -13,12 +13,12 @@
 # This will cause the build step to fail if the command has not installed correctly and can be used for testing. 
 # However, this approach will not necessarily catch issues where the installation is only accessible to the root user.
 
+# TODO accept non-local features, ie from ghcr.io (current behaviour is to treat feature as a local path and copy the folder)
 # TODO when loading scenarios.json, ensure that there are no objects in the array with matching name keys, error if so
 # TODO add optional scenario description to scenarios.json, to print alongside tests that fail, if the description is provided
 # TODO if scenarios.json param is blank, or resolves to a non existant, or invalid file, output a message explaining where the file should be and rerun with --generate-example to see an example
 # TODO accept an array of expected output strings to test for, all should be present
 # TODO pass grep options for testing expected output
-# TODO accept non-local features, ie from ghcr.io (current behaviour is to treat feature as a local path and copy the folder)
 # TODO option to test starting and executing a test script in the started dev container (rather than just building the image). start container after build, provide a command on scenarios.json to exec after container starts. Can still use the expected output and return codes on the entire build/start/exec process? will need to stop and clean up container after use. Could we also mount local .sh files to allow the exec to execute a test file, which could also report results? similar to the way devcontainer cli feature tests work? Or you could just add your validation command to exec and check for exit code and expected output, eg "exec": "mytool --version". include a test-script property on each scenario which includes a path to a test script for that scenario. Multiple scenarios might share the same test. Script should exit 0 to pass, 1 to fail
 # TODO include other .devcontainer/* artifacts (eg docker compose or docker file), for testing .devcontainer config, not just devcontainer features
 # TODO can we run some builds concurrently? How do we keep within resource limits?
