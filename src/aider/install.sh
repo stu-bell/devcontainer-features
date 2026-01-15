8c6376e4f97bded7c385c591a41fb7752e5115ac
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2026-01-15 01:17:22 +0000

    add aider

---
+++ src/aider/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copyright (c) 2026 Stuart Bell 
+# Licensed under the MIT License. See https://github.com/stu-bell/devcontainer-features/blob/main/LICENSE for license information.
+set -e
+. ./util.sh
+
+# ensure python installed
+min_py_ver=${MIN_PYTHON_VERSION:-"3.12.0"}
+install_oci_feature "ghcr.io/stu-bell/devcontainer-features/python" \
+    "VERSION=${min_py_ver}"
+
+# run install
+remote_user_run 'python -m pip install aider-install'
+remote_user_run 'aider-install'
+
+# verify version
+echo "Aider installed: $(remote_user_run 'aider --version')"
+
